e05d6eea2566406d813ae1d249e2cbe35a9677e4
Nordes <nordes@gmail.com>
2021-03-07 23:31:48 -0500

    Add trace comment

--- Flux.NewRelic/BackgroundJobs/ConfigWatchJob.cs
+++ Flux.NewRelic/BackgroundJobs/ConfigWatchJob.cs
@@ -1,5 +1,7 @@
-﻿using Flux.NewRelic.DeploymentReporter.Security;
+﻿using Flux.NewRelic.DeploymentReporter.Configurations;
+using Flux.NewRelic.DeploymentReporter.Security;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,11 +10,16 @@ namespace Flux.NewRelic.DeploymentReporter.BackgroundJobs
 {
     public class ConfigWatchJob : BackgroundService
     {
+        private readonly ILogger<ConfigWatchJob> _logger;
         private readonly IApiKeyStore _apiKeyStore;
+        private readonly ApplicationConfig _appConfig;
+        private readonly TimeSpan _loopDelay = TimeSpan.FromSeconds(30);
 
-        public ConfigWatchJob(IApiKeyStore apiKeyStore)
+        public ConfigWatchJob(ILogger<ConfigWatchJob> logger, IApiKeyStore apiKeyStore, ApplicationConfig appConfig)
         {
+            _logger = logger;
             _apiKeyStore = apiKeyStore;
+            _appConfig = appConfig;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -21,8 +28,13 @@ namespace Flux.NewRelic.DeploymentReporter.BackgroundJobs
             {
                 // Look at the appsettings.json (let's consider that one as the configuration. Maybe it will become a different one later.
 
-                // If file changed, we need to update the security store.
-                await Task.Delay(TimeSpan.FromSeconds(30), stoppingToken); 
+                // If file changed, we need to
+                // 1. update the security store.
+                // 2. update mappings
+                // 3. update other stuff... most likely
+                
+                _logger.LogTrace($"No changes, waiting {_loopDelay.TotalSeconds} seconds"); // We could use a real "System.IO.FileSystemWatcher" instead.
+                await Task.Delay(_loopDelay, stoppingToken); 
             }
         }
     }
